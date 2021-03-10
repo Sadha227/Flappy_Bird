91cfe611f52fadc8ba0053da0b807dc9596c2537
unknown <varvarchik3@gmail.com>
2021-03-10 17:24:46 +0200

    Implement ability to spawnt pipes

---
+++ Assets/Script/PipeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField] float timeToSpawner = 1f;
+    [SerializeField] GameObject pipe;
+    [SerializeField] float height;
+
+    private Coroutine spawnerPipesCoroutine;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            Vector3 randFactor = new Vector3(0, Random.Range(-height, height), 0);
+            newpipe.transform.position = transform.position + randFactor;
+            Destroy(newpipe, 5f); // 5f - Время до уничтожения // объекта-трубы (5с)
+            yield return new WaitForSeconds(timeToSpawner);
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
