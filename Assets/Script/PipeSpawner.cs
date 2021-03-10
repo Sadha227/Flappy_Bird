cb0816c6e172dc8df35100c10c2c28a428ec77a2
unknown <varvarchik3@gmail.com>
2021-03-10 18:10:26 +0200

    Implement ability to spawn and move 2 types of pipes: green and red

--- Assets/Script/PipeSpawner.cs
+++ Assets/Script/PipeSpawner.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class PipeSpawner : MonoBehaviour
 {
     [SerializeField] float timeToSpawner = 1f;
-    [SerializeField] GameObject pipe;
+    [SerializeField] GameObject pipe1;
+    [SerializeField] GameObject pipe2;
     [SerializeField] float height;
 
+    private int pipeNum = 0;
     private Coroutine spawnerPipesCoroutine;
 
     IEnumerator Start()
     {
         while (true)
         {
-            GameObject newpipe = Instantiate(pipe);
-            Vector3 randFactor = new Vector3(0, Random.Range(-height, height), 0);
-            newpipe.transform.position = transform.position + randFactor;
-            Destroy(newpipe, 5f); // 5f - Время до уничтожения // объекта-трубы (5с)
+            if (pipeNum == 0)
+            {
+                CreateAndMove(pipe1);
+               
+            }
+            else if(pipeNum == 1)
+            {
+                CreateAndMove(pipe2);
+            }
             yield return new WaitForSeconds(timeToSpawner);
+            pipeNum = 1 - pipeNum;
         }
     }
 
+    private void CreateAndMove(GameObject pipe)
+    {
+        GameObject newpipe = Instantiate(pipe);
+        Vector3 randFactor = new Vector3(0, Random.Range(-height, height), 0);
+        newpipe.transform.position = transform.position + randFactor;
+        Destroy(newpipe, 5f); // 5f - Время до уничтожения // объекта-трубы (5с)
+    }
 
     void Update()
     {
