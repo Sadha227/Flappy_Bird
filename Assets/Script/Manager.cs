4182a49469932ec34a643c9d2dc9e0edc06e7402
pp <varvarchik3@gmail.com>
2021-03-12 17:50:07 +0200

    Implement ability to collide to the ground

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
